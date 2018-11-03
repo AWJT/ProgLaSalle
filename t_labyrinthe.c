46ff4d29579cf218dce07dceba99285475c50512
Unknown <youssef550@live.com>
2018-11-02 21:28:59 -0400

    modif

--- t_labyrinthe.c
+++ t_labyrinthe.c
@@ -24,7 +24,10 @@ static int ajouter_chemin_Wilson(t_pile * chemins_W, int nb_lig, int nb_col);
 /*	              DEFINITION DES FONCTIONS PRIVEES                      */
 /************************************************************************/
 
-
+/*Cette fonction est utilisée pour obtenir une valeur « v » aléatoire qui 
+  ne fait pas déjà partie de la pile « chemins_W ». Elle reçoit la pile ainsi
+  que le nombre de valeurs (M*N).*/
+/**/
 int choix_v_chemin_Wilson(const t_pile * chemins_W, int nb_val)
 {
 
@@ -43,6 +46,21 @@ int ajouter_chemin_Wilson(t_pile * chemins_W, int nb_lig, int nb_col)
 
 t_labyrinthe init_laby(int nb_lig, int nb_col)
 {
+	int i;
+	t_labyrinthe laby;
+
+	laby.matrice_laby = (int **)malloc((2 * nb_lig + 1) * sizeof(int *));
+
+	for (i = 0; i < (2 * nb_lig + 1); i++) {
+		laby.matrice_laby[i] = (int *)malloc((2 * nb_col + 1) * sizeof(int));
+
+	}
+
+	laby.m = nb_lig;
+	laby.n = nb_col;
+	laby.nb_pos = laby.m * laby.n;
+
+	return laby;
 
 }
 
@@ -54,9 +72,17 @@ void etape_des_2piles(t_pile * chemins_W, t_pile * longueurs_W, int nb_lig, int 
 }
 
 /***********************************************************************/
-
+/*Cette procédure réalise l'algorithme donné plus haut en étape 2 et qui permet le  
+positionnement de cases LIBRE dans la matrice représentant le labyrinthe.
+L’algorithme implémenté ici vide les deux piles reçues en paramètres et réalise 
+l’assignation à LIBRE des cases consécutives aux valeurs dépilées de « chemins_W ».
+La  procédure réalise l'algorithme et ne retourne rien.
+*/
 void etape_ouverture_chemins(t_labyrinthe * lab, t_pile * chemins_W, t_pile * longueurs_W)
 {
+	while (!vide(chemins_W)) {
+
+	}
 }
 
 /***********************************************************************/
--- t_labyrinthe.h
+++ t_labyrinthe.h
@@ -5,14 +5,10 @@
 /************************************************************************/
 /* Définition pour le programme t_labyrinthe.c						    */
 
-#define NB_LIG 5
-#define NB_COL 8
-
-
 typedef struct
 {
 	//La matrice du labyrinthe
-	int matrice_laby[NB_LIG][NB_COL];
+	int ** matrice_laby;
 	//les dimensions (M, N) du labyrinthe théorique
 	int m, n;
 	//contient le nombre total de « positions » possibles (M*N)
@@ -25,12 +21,13 @@ typedef struct
 /************************************************************************/
 
 t_labyrinthe init_laby(int nb_lig, int nb_col);
-//-------------------------------------------------------------------------
+
 void etape_des_2piles(t_pile * chemins_W, t_pile * longueurs_W, int nb_lig,
 	int nb_col);
-//-------------------------------------------------------------------------
+
 void etape_ouverture_chemins(t_labyrinthe * lab, t_pile * chemins_W,
 	t_pile * longueurs_W);
-//-------------------------------------------------------------------------
+
 void creuser_une_porte(t_labyrinthe * lab, int no_contour);
-//-------------------------------------------------------------------------
+
+void detruire_laby(t_labyrinthe * laby);
